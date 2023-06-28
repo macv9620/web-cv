db4281f3d75f9ad565eff55a9f1e1925a0cbdb8c
Mateo Vasco <macv9620@utp.edu.co>
2023-06-27 21:12:19 -0500

    Se crea diseño inicial de la página

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { About } from './Components/About'
 import Contact from './Components/Contact'
+import Navbar from './Components/Navbar'
 import Projects from './Components/Projects'
 import { Skills } from './Components/Skills'
 import Testimonials from './Components/Testimonials'
@@ -9,6 +10,7 @@ function App () {
   return (
     <>
       <main className='text-gray-400 bg-gray-900 body-font'>
+        <Navbar />
         <About />
         <Projects />
         <Skills />
